8f8d72d37bc8f593dc16d4653784b688e7e2ae23
Dana <dragut_dana@yahoo.com>
2023-01-27 14:05:30 +0200

    Moved code from BeforeMethod in BeforeSuite

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import pageObjects.HomePage;
 
 public class BaseTest {
@@ -9,23 +8,32 @@ public class BaseTest {
     private WebDriver browser;
     HomePage homePage;
 
-    @BeforeMethod
-    public void setUp(){
+    @BeforeSuite
+    public void setUpSuite(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        browser= new ChromeDriver();
+        browser = new ChromeDriver();
         browser.manage().window().maximize();
+    }
+
+    @BeforeMethod
+    public void setUpTest(){
         browser.get("https://www.aboutyou.ro/");
         homePage = new HomePage(browser);
         homePage.login();
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDownTest(){
         homePage
-            .goToCartPage()
-            .deleteProducts()
-            .logout();
+                .goToCartPage()
+                .deleteProducts()
+                .logout();
 
+        browser.manage().deleteAllCookies();
+    }
+
+    @AfterSuite
+    public void tearDown(){
         browser.close();
     }
 }
